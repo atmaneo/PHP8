040a74bed2b611ea3916f18021e7009fc40d27f2
ouendadji atmane <atmane.ouenda@gmail.com>
2023-06-16 17:09:09 +0200

    PDO - CRUD - MySql

--- V9_PDO_CRUD.php
+++ V9_PDO_CRUD.php
@@ -11,24 +11,24 @@
 // comment se connecter à la BDD, ici on va tt mettre ds le mm fichier, mais la bonne pratique est de séparer le code et de faire des inclusions
 
 // on va définir des constantes d'environnement qui vont contenir les informat de connexion à ma BDD
-    define("DBHOST","localhost");    // c avec define qu'on crée des constantes. la variable DBHOST sera mon serveur (localhost)
-    define("DBUSER","root");         // user c l'utilisateur de ma BDD, sur mon serveur il s'appelle ROOT
-    define("DBPASS","");    // sur mon serveur en local il n'y a pas de mdp
-    define("DBNAME","tutos-php");    // DBNAME est le nom de ma BDD
+    define("DBHOST","localhost");       // define permet de créer des constantes. la variable DBHOST sera mon serveur (localhost)
+    define("DBUSER","root");            // user c l'utilisateur de ma BDD, sur mon serveur il s'appelle ROOT
+    define("DBPASS","");                // sur mon serveur en local il n'y a pas de mdp
+    define("DBNAME","tutos-php");       // DBNAME est le nom de ma BDD
 
 // il y a 2 façons de se connecter à 1 BDD : mysqlli (le pbe si je veux changer pour Oracle, PostGreSql je ne pourrais PAS ! il faudra changer tout le code) et PDO (php data object, on est universel on pourra changer)
 // pour PDO on va avoir besoin d'1 DSN de connexion, c 1 chaine de caractères qui va indiquer le chemin de la connexion (data source name)    
-// ici le dsn est affecté à la variable $dsn
-    $dsn = "mysql:dbname=".DBNAME.";host=".DBHOST;
+
+    $dsn = "mysql:dbname=".DBNAME.";host=".DBHOST;      // ici le dsn est affecté à la variable $dsn
 
 // on va se connecter à la base en utilisant le try catch car PDO va lever 1 exception (erreur) ki va pouvoir etre gérée par la suite.
 // essaye de faire ce kil y a ds try et sinon gère le en faisant ce kil y a dans catch; 
     try{    
-        $db = new PDO($dsn,DBUSER,DBPASS);      // on instancie PDO
-        $db->exec("SET NAMES utf8");            // execute moi la req sql qui s'appelle SET_NAMES. c 1 req sql qui dit attent tt les échanges entre mon php et ma BDD vont se faire en uft8
-
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+        $db = new PDO($dsn,DBUSER,DBPASS);              // on instancie PDO
+        $db->exec("SET NAMES utf8");            // execute moi la req sql qui s'appelle SET_NAMES. c 1 req sql qui dit attent, ts les échanges entre mon php et ma BDD vont se faire en uft8
                                                 // on s'assure d'envoyer les données en UTF8 (pour avoir les accents en BDD)
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+                                                
     }catch(PDOException $e){                    // PDO va lever 1 exception kon va appeler $e
         die($e->getMessage());      // on utilise die pour arreter le php. getMessage est 1 méthode de l'objet PDOException (ici c de la POO)
     } 
@@ -58,7 +58,15 @@
 */
         
     // SUPPRIMER UN USER
-    $sql
+    /*
+    $sql = "DELETE FROM `users` WHERE id = 51"; // info : l'opérateur de comparaison peut être remplacé par <, >
+
+        $requete = $db->query($sql);            // on execute la requete
+        */
+    // SAVOIR COMBIEN DE LIGNES ONT ETE SUPPRIMEES
+    echo $requete->rowCount();                  // m'affiche 1 car j'ai supprimé que le user id = 51
+
+    
     ?>
 </body>
 </html>
